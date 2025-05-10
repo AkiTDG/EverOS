a3702b1a04bf61ef3928ad7558c4da52e2d3cd13
AkiTDG <156631567+AkiTDG@users.noreply.github.com>
2025-05-10 16:48:49 +0800

    Create calculator.js

---
+++ src/js/features/calculator.js
@@ -0,0 +1,20 @@
+export function calculator(expression) {
+    try {
+      const cleanExpression = expression.replace(/\s+/g, '');
+
+       if (!/^[0-9+\-*/().%]+$/.test(cleanExpression)) {
+           throw new Error('Invalid characters.');
+        }
+
+        const result = new Function('return '+cleanExpression)();
+        if (result === undefined || isNaN(result)) {
+            throw new Error('Syntax error');
+        }
+        if (!isFinite(result)) {
+            throw new Error('Math error');
+        }
+        writeToConsole(`= ${result}`);
+    } catch (error) {
+        writeToConsole('Statement error');
+    }
+} 
