59a71158225ac42de2ba5cebe2bc11043e4e378c
AkiTDG <156631567+AkiTDG@users.noreply.github.com>
2025-05-11 10:35:56 +0800

    Update main.js

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,10 +1,10 @@
 import {calculator,calcUi} from '../features/calculator.js';
 import {handleCommand} from './commandHandler.js';
 import {asciitext,homeMenu,helpMenu} from './mainUI.js';
-//backbone
+
+//Console backbone
 const consoleDiv = document.getElementById('console');
 const inputField = document.getElementById('input');
-/*let currentFeature = 'home';*/
 let currentFeature = 'home';
 function getCurrentFeature() {
     return currentFeature;
@@ -13,123 +13,12 @@ function setCurrentFeature(value) {
     currentFeature = value;
 }
 
-/*
-//main UI
-const asciitext = `
- /$$$$$$$$                              /$$$$$$   /$$$$$$ 
-| $$_____/                             /$$__  $$ /$$__  $$
-| $$    /$$    /$$ /$$$$$$   /$$$$$$  | $$  \\ $$| $$  \\__/
-| $$$$$|  $$  /$$//$$__  $$ /$$__  $$ | $$  | $$|  $$$$$$ 
-| $$__/ \\  $$/$$/| $$$$$$$$| $$  \\__/ | $$  | $$ \\____  $$
-| $$     \\  $$$/ | $$_____/| $$       | $$  | $$ /$$  \\ $$
-| $$$$$$$$\\  $/  |  $$$$$$$| $$       |  $$$$$$/|  $$$$$$/
-|________/ \\_/    \\_______/|__/        \\______/  \\______/ 
-`;
-const homeMenu = `
-+--------------------------------------------+
-|============================================|                                            
-| Features & (command name):                 |
-|                                            |
-| [1.Simple Calculator          (calc)]      |
-| [2.Metric system converter     (msc)]      |
-| [3.Temperature converter        (tc)]      |
-| [4.Day/Time converter          (dtc)]      |
-|                                            |
-| Type "help" for important commands         |
-| Press F5 (Or refresh icon)to restart OS    |
-|============================================|                                            
-+--------------------------------------------+`;
-
-//help menu
-const helpText = `
-+---------------------------------------------------------------------------------+
-|=================================================================================|
-| Available commands:                                                             |
-| [nav [feature]: Navigate to a feature (e.g. nav calc)         ]                 |
-| [exit: Exit current feature                                   ]                 |
-| [nav home: Go to home menu                                    ]                 |
-| [clear: Clear the console                                     ]                 |                                           
-|                                                                                 |                                           
-| Feature functions:                                                              |                                           
-| note: Features with * are under implementation                                  |                                            
-| [1. Simple Calculator (calc)]: Calculates simple mathematical problems          |                                            
-| [2. Metric system converter (msc)]*: Calculates any metric system measurement   |
-| [3. Temperature converter (tc)]*: Converts degree to fahrenheit & vice versa    |
-| [4. Day/Time converter (dtc)]*: Calculates the measurement of time              |                                                                               
-| in its equivalents (e.g., days to hours and vice-versa)                         |
-|=================================================================================|                                                                                                                       
-+---------------------------------------------------------------------------------+ 
-`;*/
-
 //U.I. backbone
 function writeToConsole(text) {
     const consoleDiv = document.getElementById('console');
     consoleDiv.textContent += text + '\n';
     consoleDiv.scrollTop = consoleDiv.scrollHeight;
 }window.writeToConsole = writeToConsole;
-
-//logic function
-/*function handleCommand(rawInput) {
-    const command = rawInput.trim();
-    const lowerCommand = command.toLowerCase();
-    if (command === '') return;
-    if (currentFeature !== 'home' || currentFeature !== 'exit') {
-        if (lowerCommand === 'exit') {
-            currentFeature = 'exit';
-            writeToConsole('Feature closed successfully.');
-            return;
-        }
-         if (lowerCommand === 'help') {
-            writeToConsole(helpText);
-            return;
-        }
-        if (lowerCommand === 'clear') {
-        consoleDiv.textContent = '';
-        return;
-        }
-        if (currentFeature === 'calculator') {
-            calculator(command);
-            return;
-        }
-    writeToConsole('You must exit the current feature before using other commands.');
-    return;
-    }  
-    if (lowerCommand.startsWith('nav ')) {
-        const target = lowerCommand.substring(4);
-        switch (target) {
-            case 'home':
-                currentFeature = 'home';
-                writeToConsole(homeMenu);
-                break;
-            case 'help':
-                writeToConsole(helpText);
-                break;
-            case 'calc':
-                currentFeature = 'calculator';
-                writeToConsole(calcUi);
-                break;
-            case 'msc':
-                currentFeature = 'Metric system converter';
-                writeToConsole('This feature is under implementation');
-                break;
-            case 'tc':
-                currentFeature = 'Temperature converter';
-                writeToConsole('This feature is under implementation');
-                break;   
-            case 'dtc':
-                currentFeature = 'Day/Time converter';
-                writeToConsole('This feature is under implementation');
-                break; 
-            default:
-                writeToConsole('Unknown feature. Type "help" for available commands.');
-        }
-        return;
-    }
-    
-writeToConsole('Unknown command or wrong context. Type "help" for assistance.');          
-}*/
-
- 
 
 //input logic
 inputField.addEventListener('keydown', function(event) {
@@ -152,7 +41,6 @@ inputField.addEventListener('keydown', function(event) {
         inputField.value = '';
     }
 });
-
 
 writeToConsole(asciitext);
 writeToConsole('+--------------------------------------------------+');
