8876710db837d3587e6f2598cd43ebaccd0e6111
AkiTDG <156631567+AkiTDG@users.noreply.github.com>
2025-05-11 10:58:43 +0800

    Integrated some appearances to home menu

--- src/js/main/mainUI.js
+++ src/js/main/mainUI.js
@@ -1,4 +1,4 @@
-//main UI
+/*//main UI
 export const asciitext = `
  /$$$$$$$$                              /$$$$$$   /$$$$$$ 
 | $$_____/                             /$$__  $$ /$$__  $$
@@ -22,7 +22,46 @@ export const homeMenu = `
 | Type "help" for important commands         |
 | Press F5 (Or refresh icon)to restart OS    |
 |============================================|                                            
-+--------------------------------------------+`;
++--------------------------------------------+`;*/
+
+export function homeMenu(){
+   const date = new Date().toLocaleDateString();
+    const time = new Date().toLocaleTimeString(undefined, {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+   return `
+  /$$$$$$$$                              /$$$$$$   /$$$$$$ 
+| $$_____/                             /$$__  $$ /$$__  $$
+| $$    /$$    /$$ /$$$$$$   /$$$$$$  | $$  \\ $$| $$  \\__/
+| $$$$$|  $$  /$$//$$__  $$ /$$__  $$ | $$  | $$|  $$$$$$ 
+| $$__/ \\  $$/$$/| $$$$$$$$| $$  \\__/ | $$  | $$ \\____  $$
+| $$     \\  $$$/ | $$_____/| $$       | $$  | $$ /$$  \\ $$
+| $$$$$$$$\\  $/  |  $$$$$$$| $$       |  $$$$$$/|  $$$$$$/
+|________/ \\_/    \\_______/|__/        \\______/  \\______/
+
++--------------------------------------------------+
+|==================================================|
+|        =======[Welcome to EverOS!]=======        |
+| Today is:   ${date.padEnd(37)}|
+| Time check: ${time.padEnd(37)}|
+|==================================================|
++--------------------------------------------------+
+
++--------------------------------------------+
+|============================================|                                            
+| Features & (command name):                 |
+|                                            |
+| [1.Simple Calculator          (calc)]      |
+| [2.Metric system converter     (msc)]      |
+| [3.Temperature converter        (tc)]      |
+| [4.Day/Time converter          (dtc)]      |
+|                                            |
+| Type "help" for important commands         |
+| Press F5 (Or refresh icon) to restart OS   |
+|============================================|                                            
++--------------------------------------------+`;   
+}
 
 //help menu
 export const helpMenu = `
