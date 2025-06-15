daca82e133b974f73e5703a61b4f37742c932e20
AkiTDG <156631567+AkiTDG@users.noreply.github.com>
2025-06-15 12:00:03 +0800

    Add files via upload

--- src/js/features/calculator/supabaseCalcDB.js
+++ src/js/features/calculator/supabaseCalcDB.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://zmnlctsroufobhdyntsw.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptbmxjdHNyb3Vmb2JoZHludHN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MDEwOTIsImV4cCI6MjA2NTQ3NzA5Mn0.rG71SRERVjnIrQDcQbqGKwiFUsO3mhQ37bPXnFrenjM'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
